b3e723d7d0cad3754f7b6c2fb79b678f17117a8f
unHealthy <chenzhutian1992@gmail.com>
2015-11-28 15:04:50 +0800

    try my demo based on py3

---
+++ vn.myTest/MyApp.py
@@ -0,0 +1,18 @@
+﻿# -*- coding:utf-8 -*- 
+import tushare as ts
+import sys
+
+class MyApp(object):
+    """description of class"""
+    
+    def __init__(self):
+        print('HI this is me')
+        print('大家好这是我')
+        df = ts.get_today_all()
+        print(df)
+
+def main():
+    myapp = MyApp()
+
+if __name__ == '__main__':
+    main()
